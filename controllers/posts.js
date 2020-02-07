c11f85aa8978b47a4b9f56336f9b068564c70a80
jpettett <joseph.pettett@gmail.com>
2020-02-06 18:11:22 -0800

    crud complete

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Post = require('../models/schema');
 const router = express.Router();
 
+// shows all posts //
 router.get('/', (req, res) => {
   Post.find({})
     .then(posts => {
@@ -10,28 +11,44 @@ router.get('/', (req, res) => {
     .catch(console.error);
 });
 
+// post new //
 router.get('/submit', (req, res) => {
   res.render('submit');
 });
-
 router.post('/', (req, res) => {
   Post.create(req.body)
     .then(newPost => {
       res.redirect('/posts');
-      // res.json(newPost);
     })
     .catch(console.error);
 });
 
+// delete //
 router.delete('/:id', (req, res) => {
   Post.findOneAndDelete({ _id: req.params.id }).then(() => {
     res.redirect('/posts');
   });
 });
 
+// edit function //
 router.get('/:id/edit', (req, res) => {
   Post.findById(req.params.id).then(post => {
     res.render('edit', post);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  const post = {
+    description: req.body.description
+  };
+  Post.findOneAndUpdate(
+    {
+      _id: req.params.id
+    },
+    post,
+    { new: true }
+  ).then(post => {
+    res.redirect('/posts');
   });
 });
 
